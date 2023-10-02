122d6942585aedda6b139fdd1fdd5d0bcafa6021
JNeto <joaomendes15@gmail.com>
2023-10-02 02:14:22 +0100

    Vertical terminal velocity added, The player now has a limit of how quick they can fall

--- Assets/Scripts/MonoBehaviours/Player/PlayerController.cs
+++ Assets/Scripts/MonoBehaviours/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 [RequireComponent(typeof(CharacterController))]
@@ -27,9 +28,9 @@ public class PlayerController : MonoBehaviour
     [Header("Custom Gravity")]
     [SerializeField, Range(1, 100)] private float _gravityForce = 30f;
     [SerializeField, Range(0.1f, 3f), Tooltip(MultTip)] private float _gravityMultiplier = 1;
+    [SerializeField, Range(-300, -1)] private float _terminalFallVelocity = -50f;
     private const string MultTip = "How stong gravity feels like (1 is good)";
     
-
     private void Awake()
     {
         CanMove = true;
@@ -73,16 +74,27 @@ public class PlayerController : MonoBehaviour
         // Resets of vertical velocity when on the ground to prevent accumulation,
         // with is a non-0 value to prevent character controller failures like the ground detector not working properly.
         // this is important to ensure that the character doesn't start floating after landing.
-        if (!_characterController.isGrounded) VerticalVelocity -= _gravityMultiplier * _gravityForce * Time.deltaTime;
-        else if (!_jumpInput) VerticalVelocity = GroundedDecrement; 
-        
+        if (!_characterController.isGrounded)
+        {
+            // Calculates an acceleration corresponding to my custom gravity.
+            VerticalVelocity -= _gravityMultiplier * _gravityForce * Time.deltaTime;
+            
+            // Checks for Terminal Velocity in Fall, and overrides it if required.
+            if (VerticalVelocity < _terminalFallVelocity)
+                VerticalVelocity = _terminalFallVelocity;
+        }
+        else if (!_jumpInput)
+        {
+            VerticalVelocity = GroundedDecrement;
+        }
+
         // Resets the jump input.
         _jumpInput = false; 
         
         // Applies the vertical velocity to the movement. Delta Time is required because its a constant force.
         delta.y += VerticalVelocity * Time.deltaTime;
         
-        // Moves the character
+        // Moves the character by applying the movement delta to it.
         _characterController.Move(delta);
     }
     
