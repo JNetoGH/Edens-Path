8bbf5a929ba5e1fe0ed33729937b222aa284f75a
JNeto <joaomendes15@gmail.com>
2023-10-29 22:55:19 +0000

    flora cutscene added

--- Assets/Scripts/MonoBehaviours/CutsceneControllersAndTriggres/FloraCutsceneController.cs
+++ Assets/Scripts/MonoBehaviours/CutsceneControllersAndTriggres/FloraCutsceneController.cs
@@ -6,21 +6,40 @@ public class FloraCutsceneController : ACutsceneController
 {
     
     [BoxGroup, Required, SerializeField] private GameObject _flora;
+    [BoxGroup, Required, SerializeField] private AudioClip _floraDialogue1;
+    [BoxGroup, Required, SerializeField] private GameObject _floraNpcInteraction1;
+    [BoxGroup, Required, SerializeField] private GameObject _floraNpcInteractionMsg;
     private AudioSource _audioSource;
-
+    
+    
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
     
     public override void PlayCutscene()
     {
-        GameObject playerCamera = FindObjectOfType<CameraController>().gameObject;
-        
-        hasBeenAlreadyWatched = true;
-        _audioSource.Play();
-        
-        GameManager.EnterCutsceneMode();
+        animator.SetTrigger("NextCutscutscene");
+        PlayDialog1Audio();
+        DisableFloraNpcInteractionTrigger1();
     }
+
+    #region Animation Events
+    
+        public void PlayDialog1Audio()
+        {
+            _audioSource.clip = _floraDialogue1;
+            _audioSource.Play();
+        }
+
+        public void DisableFloraNpcInteractionTrigger1()
+        {
+            _floraNpcInteraction1.SetActive(false);
+            _floraNpcInteractionMsg.SetActive(false);
+        }
+        
+    #endregion
+   
     
 }
