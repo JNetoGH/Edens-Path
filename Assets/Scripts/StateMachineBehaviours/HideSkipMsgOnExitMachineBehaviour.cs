fe44696d8965ff68680212271e4cd230f1bd317c
JNeto <joaomendes15@gmail.com>
2023-11-19 05:21:12 +0000

    new assets set

--- Assets/Scripts/StateMachineBehaviours/HideSkipMsgOnExitMachineBehaviour.cs
+++ Assets/Scripts/StateMachineBehaviours/HideSkipMsgOnExitMachineBehaviour.cs
@@ -6,7 +6,7 @@ public class HideSkipMsgOnExitMachineBehaviour : StateMachineBehaviour
     
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
     {
-        GameManager.SkipCutsceneMsg.SetActive(true);
+        GameManager.SkipCutsceneMsg.SetActive(false);
     }
     
 }
