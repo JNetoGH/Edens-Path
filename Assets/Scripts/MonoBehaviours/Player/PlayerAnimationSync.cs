d7e3e748ff95bb11f58862b6e63319050afc5893
JNeto <joaomendes15@gmail.com>
2023-12-12 20:44:30 +0000

    player animations polished

--- Assets/Scripts/MonoBehaviours/Player/PlayerAnimationSync.cs
+++ Assets/Scripts/MonoBehaviours/Player/PlayerAnimationSync.cs
@@ -8,14 +8,15 @@ public class PlayerAnimationSync : MonoBehaviour
     [SerializeField, Required] private Animator _animator;
    
     // Components
-    private PickupSystem _pickupSystem;
     private PlayerController _playerController;
     private GroundDetector _groundDetector;
+    private PickupSystem _pickupSystem;
 
     // Animator Parameters
     private static readonly int IsWalking = Animator.StringToHash("isWalking");
     private static readonly int IsPickingUp = Animator.StringToHash("isPickingUp");
     private static readonly int IsOutlining = Animator.StringToHash("isOutlining");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
     private static readonly int Jump = Animator.StringToHash("jump");
 
     // Start is called before the first frame update
@@ -36,6 +37,7 @@ public class PlayerAnimationSync : MonoBehaviour
         } 
         _animator.SetBool(IsOutlining, _pickupSystem.IsOutlining);
         _animator.SetBool(IsPickingUp, _pickupSystem.IsPickingUp);
+        _animator.SetBool(IsGrounded, _groundDetector.IsGrounded && _playerController.VerticalVelocity < 0.1f);
         _animator.SetBool(IsWalking, _playerController.IsMoving && _groundDetector.IsGrounded);
         if (_playerController.JumpedThisFrame)
             _animator.SetTrigger(Jump);
